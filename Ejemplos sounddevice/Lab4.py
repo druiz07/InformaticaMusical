f874b18910c7f4af4bbf601916d828264337edb3
druiz07 <druiz07@ucm.es>
2022-03-02 16:56:01 +0100

    Intento de ejercicio 5 aunque ni si quiera esta probado wtf

--- Ejemplos sounddevice/Lab4.py
+++ Ejemplos sounddevice/Lab4.py
@@ -69,3 +69,91 @@ while c!='q':
 stream.stop()
 
 # %%
+
+import numpy as np         # arrays    
+import sounddevice as sd   # modulo de conexión con portAudio
+import soundfile as sf     # para lectura/escritura de wavs
+import kbhit
+import os  
+import pygame
+from pygame.locals import *   
+
+
+def KarplusStrong(frec, dur):
+ N = SRATE // int(frec) # la frecuencia determina el tamanio del buffer
+ buf = np.random.rand(N) * 2 - 1 # buffer inicial: ruido
+ nSamples = int(dur*SRATE)
+
+ samples = np.empty(nSamples, dtype=np.float32) # salida
+
+ # generamos los nSamples haciendo recorrido circular por el buffer
+ for i in range(nSamples):
+     samples[i] = buf[i % N] # recorrido de buffer circular
+     buf[i % N] = 0.5 * (buf[i % N] + buf[(1 + i) % N]) # filtrado
+     return samples
+
+stream = sd.OutputStream(samplerate=SRATE,blocksize=CHUNK,channels=1)  
+stream.start()
+stream.write(np.float32(KarplusStrong(1000,2)))
+
+
+
+# %%
+#Intento de juntar una tabla de ondas con el algoritmo 
+
+import numpy as np         # arrays    
+import sounddevice as sd   # modulo de conexión con portAudio
+import soundfile as sf     # para lectura/escritura de wavs
+import kbhit
+import os  
+import pygame
+from pygame.locals import *          
+
+
+SRATE = 44100       # sampling rate, Hz, must be integer
+CHUNK = 1024
+WIDTH = 800
+HEIGHT = 300
+
+#Algoritmo usando una tabla de ondas
+def karplus_strong(wavetable, dur):
+    """Synthesizes a new waveform from an existing wavetable, modifies last sample by averaging."""
+    samples = []
+    current_sample = 0
+    previous_value = 0
+    n_samples= int(dur*SRATE)
+
+    while len(samples) < n_samples:
+        wavetable[current_sample] = 0.5 * (wavetable[current_sample] + previous_value)
+        samples.append(wavetable[current_sample])
+        previous_value = samples[-1]
+        current_sample += 1
+        current_sample = current_sample % wavetable.size
+    return np.array(samples)
+
+
+def synthWaveTable(wavetable, frame):
+    samples = np.zeros(CHUNK)
+    t = frame % len(wavetable)
+    for i in range(CHUNK):
+        samples[i] = wavetable[t]
+        t = (t+1) % len(wavetable)
+        return samples
+
+    
+stream = sd.OutputStream(samplerate=SRATE,blocksize=CHUNK,channels=1)  
+stream.start()
+
+kb = kbhit.KBHit()
+c = ' '
+#Probamos con un seno de 800 hz
+frec=800
+waveTable = np.sin(2*np.pi*frec*np.arange(SRATE/frec)/SRATE)
+
+while c!='q':   
+    samples=synthWaveTable(waveTable,frame)
+    stream.write(np.float32(0.5*karplus_strong(samples,1))) 
+   
+   
+
+stream.stop()
